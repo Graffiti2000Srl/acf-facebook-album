6094d3811e094cc3542c1b301bdad603bddd6d17
Zweer <flicofloc@gmail.com>
2014-03-20 16:33:20 +0100

    Now it should work with the page URI and not the page ID

--- facebook-album.php
+++ facebook-album.php
@@ -1,127 +1,139 @@
 <?php
 
-class acf_field_facebook_album extends acf_field 
+class acf_field_facebook_album extends acf_field
 {
-	public $settings;
-	
-	public $defaults;
-	
-	public function __construct()
-	{
-		$this->name     = 'facebook_album';
-		$this->label    = __('Facebook Album');
-		$this->category = __('Choice', 'acf');
-	
-		$this->defaults = array(
-			'allow_null'    => 1,
-			'facebook_page' => '',
-		);
-	
-		parent::__construct();
-	
-		$this->settings = array(
-			'path'    => apply_filters('acf/helpers/get_path', __FILE__),
-			'dir'     => apply_filters('acf/helpers/get_dir', __FILE__),
-			'version' => '0.1',
-		);
-	}
-	
-	public function create_options($field)
-	{
-		$field = array_merge($this->defaults, $field);
-		$key   = $field['name'];
-		?>
-<tr class="field_option field_option_<?php echo $this->name; ?>">
-	<td class="label">
-		<label><?php _e('Facebook Page URI', 'acf'); ?></label>
-		<p class="description"><?php _e('Your Facebook Page URI', 'acf'); ?></p>
-	</td>
-	<td>
-		<?php
+    public $settings;
 
-		do_action('acf/create_field', array(
-			'type'  => 'text',
-			'name'  => 'fields['.$key.'][facebook_page]',
-			'value' => $field['facebook_page'],
-		));
+    public $defaults;
 
-		?>
-	</td>
-</tr>
-<tr class="field_option field_option_<?php echo $this->name; ?>">
-	<td class="label">
-		<label><?php _e('Allow Null?', 'acf'); ?></label>
-	</td>
-	<td>
-		<?php
+    public function __construct()
+    {
+        $this->name     = 'facebook_album';
+        $this->label    = __('Facebook Album');
+        $this->category = __('Choice', 'acf');
 
-		do_action('acf/create_field', array(
-			'type'	  => 'radio',
-			'name'	  => 'fields['.$key.'][allow_null]',
-			'value'	  => $field['allow_null'],
-			'choices' => array(
-				1 => __("Yes", 'acf'),
-				0 => __("No", 'acf'),
-			),
-			'layout'  => 'horizontal',
-		));
+        $this->defaults = array(
+            'allow_null'    => 1,
+            'facebook_page' => '',
+        );
 
-		?>
-	</td>
-</tr>
-		<?php
-	}
+        parent::__construct();
 
-	public function create_field($field)
-	{
-		$field = array_merge($this->defaults, $field); $albums = array(); $continue = false;
-		$data = json_decode(file_get_contents('https://graph.facebook.com/'.$field['facebook_page'].'/albums?fields=name'), true);
-		if ($data) {
-			do {
-				if (count($data)) {
-					$albums = array_merge($albums, (array) $data['data']);
-					if ($continue = array_key_exists('next', $data['paging'])) {
-						$data = json_decode(file_get_contents($data['paging']['next']), true);
-					}
-				} else {
-					$continue = false;
-				}
-			} while ($continue);
+        $this->settings = array(
+            'path'    => apply_filters('acf/helpers/get_path', __FILE__),
+            'dir'     => apply_filters('acf/helpers/get_dir', __FILE__),
+            'version' => '0.1',
+        );
+    }
 
-			// html
-			echo '<select id="' . $field['id'] . '" class="' . $field['class'] . '" name="' . $field['name'] . '" ' . ' >';
+    public function create_options($field)
+    {
+        $field = array_merge($this->defaults, $field);
+        $key   = $field['name'];
+        ?>
+        <tr class="field_option field_option_<?php echo $this->name; ?>">
+            <td class="label">
+                <label><?php _e('Facebook Page URI', 'acf'); ?></label>
+                <p class="description"><?php _e('Your Facebook Page URI', 'acf'); ?></p>
+            </td>
+            <td>
+                <?php
 
-			// null
-			if( $field['allow_null'] )
-			{
-				echo '<option value="null">- ' . __('Select an Album', 'acf') . ' -</option>';
-			}
+                do_action('acf/create_field', array(
+                    'type'  => 'text',
+                    'name'  => 'fields['.$key.'][facebook_page]',
+                    'value' => $field['facebook_page'],
+                ));
 
-			// loop through values and add them as options
-			if( is_array($albums) )
-			{
-				foreach( $albums as $album )
-				{
-					$selected = in_array($album['id'], $field['value']) ? 'selected="selected"' : '';
-					echo '<option value="'.$album['id'].'" '.$selected.'>'.$album['name'].'</option>';
-				}
-			}
+                ?>
+            </td>
+        </tr>
+        <tr class="field_option field_option_<?php echo $this->name; ?>">
+            <td class="label">
+                <label><?php _e('Allow Null?', 'acf'); ?></label>
+            </td>
+            <td>
+                <?php
 
-			echo '</select>';
-		} else {
-			echo '<p>Your plugin is misconfigured. Set your Facebook Page ID.</p>';
-		}
-	}
+                do_action('acf/create_field', array(
+                    'type'	  => 'radio',
+                    'name'	  => 'fields['.$key.'][allow_null]',
+                    'value'	  => $field['allow_null'],
+                    'choices' => array(
+                        1 => __("Yes", 'acf'),
+                        0 => __("No", 'acf'),
+                    ),
+                    'layout'  => 'horizontal',
+                ));
 
-	public function format_value($value, $post_id, $field)
-	{
-		return $value;
-	}
+                ?>
+            </td>
+        </tr>
+    <?php
+    }
 
-	public function format_value_for_api($value, $post_id, $field)
-	{
-		return $value;
-	}
+    public function create_field($field)
+    {
+        $field    = array_merge($this->defaults, $field);
+        $albums   = array();
+        $continue = false;
+
+        $page     = $field['facebook_page'];
+        $pageName = substr(strrchr(trim($page, '/'), '/'),1);
+        $pageID   = json_decode(file_get_contents('https://graph.facebook.com/' . $pageName . '?fields=id'));
+
+        if (isset($pageID)) {
+            $pageID = $pageID->id;
+            $data   = json_decode(file_get_contents('https://graph.facebook.com/' . $pageID . '/albums?fields=name'), true);
+
+            if ($data) {
+                do {
+                    if (count($data)) {
+                        $albums = array_merge($albums, (array) $data['data']);
+                        if ($continue = array_key_exists('next', $data['paging'])) {
+                            $data = json_decode(file_get_contents($data['paging']['next']), true);
+                        }
+                    } else {
+                        $continue = false;
+                    }
+                } while ($continue);
+
+                // html
+                echo '<select id="' . $field['id'] . '" class="' . $field['class'] . '" name="' . $field['name'] . '" ' . '>';
+
+                // null
+                if($field['allow_null'])
+                {
+                    echo '<option value="null">- ' . __('Select an Album', 'acf') . ' -</option>';
+                }
+
+                // loop through values and add them as options
+                if (is_array($albums))
+                {
+                    foreach ($albums as $album)
+                    {
+                        echo '<option value="' . $album['id'] . '"' . ($album['id'] == $field['value'] ? ' selected="selected"' : '') . '>' . $album['name'] . '</option>';
+                    }
+                }
+
+                echo '</select>';
+            } else {
+                echo '<p>Your plugin is misconfigured. Set your Facebook Page URI.</p>';
+            }
+        } else {
+            echo '<p>Your plugin is misconfigured. Set your Facebook Page URI.</p>';
+        }
+    }
+
+    public function format_value($value, $post_id, $field)
+    {
+        return $value;
+    }
+
+    public function format_value_for_api($value, $post_id, $field)
+    {
+        return $value;
+    }
 }
 
 
